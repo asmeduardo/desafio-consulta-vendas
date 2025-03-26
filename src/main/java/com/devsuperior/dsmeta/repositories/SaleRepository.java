53e5082e0d65fc525a4d3df9a1fe830175b323c1
Eduardo Melo <eduardo.melo01992@gmail.com>
2025-03-26 15:01:04 -0300

    Resolução do desafio

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,39 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleReportDTO(obj.id, obj.date, obj.amount, obj.seller.name AS sellerName) " +
+                    "FROM Sale obj " +
+                    "WHERE obj.date BETWEEN :minDate AND :maxDate " +
+                    "AND (:name IS NULL OR LOWER(obj.seller.name) LIKE LOWER(CONCAT('%', :name, '%')))")
+    Page<SaleReportDTO> getSalesReport(
+            LocalDate minDate,
+            LocalDate maxDate,
+            String name,
+            Pageable pageable
+    );
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name AS sellerName, " +
+            "CAST(SUM(obj.amount) AS double) AS total) " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :minDate AND :maxDate " +
+            "GROUP BY obj.seller.name")
+    List<SaleSummaryDTO> getSalesSummary(
+            LocalDate minDate,
+            LocalDate maxDate
+    );
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,16 +1,30 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SaleService {
+
+	private static final LocalDate TODAY = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+	private static final LocalDate ONE_YEAR_AGO = TODAY.minusYears(1L);
+	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE;
 
 	@Autowired
 	private SaleRepository repository;
@@ -19,5 +33,23 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<SaleReportDTO> getReport(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate parsedMinDate = parseOrDefault(minDate, ONE_YEAR_AGO);
+		LocalDate parsedMaxDate = parseOrDefault(maxDate, TODAY);
+		return repository.getSalesReport(parsedMinDate, parsedMaxDate, name, pageable);
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSummaryDTO> getSummary(String minDate, String maxDate) {
+		LocalDate parsedMinDate = parseOrDefault(minDate, ONE_YEAR_AGO);
+		LocalDate parsedMaxDate = parseOrDefault(maxDate, TODAY);
+		return repository.getSalesSummary(parsedMinDate, parsedMaxDate);
+	}
+
+	private LocalDate parseOrDefault(String date, LocalDate defaultDate) {
+		return (date == null || date.isEmpty()) ? defaultDate : LocalDate.parse(date, DATE_FORMATTER);
 	}
 }
